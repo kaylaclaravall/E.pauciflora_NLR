f5f6b2f46860fdeb3679823d4601f67d9d13b16d
Kyle Claravall <62874933+kyleclaravall@users.noreply.github.com>
2020-05-02 09:52:20 +1000

    Add files via upload

---
+++ make_upper.py
@@ -0,0 +1,22 @@
+# make_upper.py
+
+# Enter the following code into command line to run this script:
+# python maker_upper.py input.fasta > output.fasta
+
+import sys
+
+fastaFileName = sys.argv[1]
+fasta_list = []
+with open(fastaFileName) as fastaFile:
+    fasta_list = fastaFile.read().splitlines()
+
+new_list = []
+
+for entry in fasta_list:
+    if ">" in entry:
+        new_list.append(entry)
+    else:
+        new_list.append(entry.upper())
+
+for entry in new_list:
+    print(entry)
